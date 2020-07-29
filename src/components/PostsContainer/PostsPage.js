97f993548e0be5bf05b52e268e3d1665b6c47604
ZackNemec <zacknemec@live.com>
2020-07-28 17:56:27 -0700

    working on search

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,13 +3,21 @@
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
+  console.log(dummyData);
   // set up state for your data
+  const [post] = React.useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {
+        /* map through data here to return a Post and pass data as props to Post */
+        post.map((object, index) => {
+          return <Post key={index} post={object} />;
+        })
+      }
+      ;
     </div>
   );
 };
--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,21 +1,29 @@
 // You do not need to change any code in this file
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faCircle,
+  faCompass,
+} from "@fortawesome/free-regular-svg-icons";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [search, setSearch] = React.useState("");
+  const SearchChange = (e) => {
+    setSearch(e.target.value);
+
+    console.log(search);
+  };
+
   return (
     <div className="search-bar-wrapper">
       <div className="social">
         <FontAwesomeIcon icon={faInstagram} />
       </div>
       <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+        <input type="text" placeholder="Search" onChange={SearchChange} />
       </form>
       <div className="social-wrapper">
         <div className="social">
